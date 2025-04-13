3aaf5d75422c1acc88b78959c88b1954283760fb
Soshi1 <saroshibrar@gmail.com>
2025-04-14 00:03:44 +0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        number = 10;
+        String name;
+        name = "Sarosh";
+
+        Console.WriteLine(number);
+        Console.WriteLine(name);
+    }
+}
